bf773c20140af48ce8711ca7823c6c7acbb36a23
Rehman <xyz@gmail.com>
2022-03-23 09:37:24 +0000

    big3 and rev

--- big3.c
+++ big3.c
@@ -1,31 +1,13 @@
 #include <stdio.h>
-
-int main() {
-
-  double n1, n2, n3;
-
-  printf("Enter three numbers: ");
-  scanf("%lf %lf %lf", &n1, &n2, &n3);
-
-  // outer if statement
-  if (n1 >= n2) {
-
-    // inner if...else
-    if (n1 >= n3)
-      printf("%.2lf is the largest number.", n1);
-    else
-      printf("%.2lf is the largest number.", n3);
-  }
-
-  // outer else statement
-  else {
-
-    // inner if...else
-    if (n2 >= n3)
-      printf("%.2lf is the largest number.", n2);
-    else
-      printf("%.2lf is the largest number.", n3);
-  }
-
-  return 0;
+int bigg() {
+    double n1, n2, n3;
+    printf("Enter three different numbers: ");
+    scanf("%lf %lf %lf", &n1, &n2, &n3);
+    if (n1 >= n2 && n1 >= n3)
+        printf("%.2f is the largest number.", n1);
+    if (n2 >= n1 && n2 >= n3)
+        printf("%.2f is the largest number.", n2);
+    if (n3 >= n1 && n3 >= n2)
+        printf("%.2f is the largest number.", n3);
+//    return 0;
 }
